e4a9b08d17f339ae90ea7f5ef8568c6ba9dea9ec
Nick Khurana <nick@bendaygo.com>
2019-12-21 17:14:42 -0500

    sheldon and leanord recognition

--- js/content.js
+++ js/content.js
@@ -1,5 +1,6 @@
 $ = jQuery;
 MODEL_URL = 'https://benerdy.net/models';
+IMAGE_URL = 'https://benerdy.net/images';
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.method == "clickAction") {
@@ -43,9 +44,51 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             canvas2.width = $video.width();
             canvas2.height = $video.height();
             context2.drawImage(video, 0, 0, canvas2.width, canvas2.height);
-            faceapi.detectAllFaces(canvas2).withFaceLandmarks().withFaceDescriptors().then(function(fullFaceDescriptions) {
-              faceapi.draw.drawDetections(canvas, fullFaceDescriptions);
+
+            // rendering boxes around all faces.
+            var fullFaceDescriptions = null;
+            faceapi.detectAllFaces(canvas2).withFaceLandmarks().withFaceDescriptors().then(function(result) {
+              fullFaceDescriptions = result;
+              //faceapi.draw.drawDetections(canvas, fullFaceDescriptions);
+              //faceapi.draw.drawFaceLandmarks(canvas, fullFaceDescriptions);
             });
+
+            function runFaceMatchStuff(labeledFaceDescriptors) {
+              if(!fullFaceDescriptions) return;
+              var maxDescriptorDistance = 0.6;
+              var faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance);
+              var results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor));
+              results.forEach((bestMatch, i) => {
+                var box = fullFaceDescriptions[i].detection.box
+                var text = bestMatch.toString()
+                var drawBox = new faceapi.draw.DrawBox(box, { label: text })
+                drawBox.draw(canvas)
+              });
+            }
+
+            var index = 0;
+            var labels = ['sheldon1','leonard1'];
+            var labeledFaceDescriptors = [];
+            function iterateLabels(index) {
+              var label = labels[index];
+              if(!label) {
+                runFaceMatchStuff(labeledFaceDescriptors);
+                return;
+              }
+              var imgUrl = `${IMAGE_URL}/${label}.png`
+              faceapi.fetchImage(imgUrl).then(function(img) {
+                faceapi.computeFaceDescriptor(img).then(function(descriptors) {
+                  if(descriptors) {
+                    labeledFaceDescriptors.push(new faceapi.LabeledFaceDescriptors(label, [descriptors]));
+                    iterateLabels((index += 1));
+                  } else {
+                    console.log("no faces detected for ", label);
+                    iterateLabels((index += 1));
+                  }
+                });
+              });
+            }
+            iterateLabels(index);
           });
         });
       });
