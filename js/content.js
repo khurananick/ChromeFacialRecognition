2bfa5beb6a890a0f919f4bdcfe617e9c9c073657
Nick Khurana <nick@bendaygo.com>
2019-12-24 12:22:52 -0500

    facediv.js

--- js/content.js
+++ js/content.js
@@ -3,33 +3,6 @@ $ = jQuery;
 let PEOPLE = {};
 
 // load a div for each unique face detected
-function renderFaceDiv(faceKey) {
-  var arr = Object.values(PEOPLE);
-  arr = arr.sort((a, b) => (a.timestamp > b.timestamp) ? -1 : 1);
-
-  var overlay = $("#overlay");
-  overlay.html("");
-
-  for(var index in PEOPLE) {
-    var person = PEOPLE[index];
-    var faceDiv = document.createElement("div")
-    var linkToImdb = document.createElement("a")
-    var nameOfFace = document.createTextNode(person.name)
-
-    faceDiv.style.border = "1px solid blue"
-    faceDiv.style.padding = "15pt"
-    linkToImdb.href = person.imdb_url
-    linkToImdb.target = '_blank'
-
-    // add content to faceDiv
-    faceDiv.appendChild(linkToImdb)
-    linkToImdb.appendChild(nameOfFace)
-
-    // add faceDiv to overlay
-    overlay.append(faceDiv)
-  }
-}
-
 function startRecognitionInVideo() {
   // get references to required items on screen.
   var $video = $("video");
