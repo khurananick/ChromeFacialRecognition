4eac97a12fe0490c0131b3214a1b280601034a81
Nick Khurana <nick@bendaygo.com>
2019-12-24 12:08:19 -0500

    incorporated faceDiv overlay

--- js/content.js
+++ js/content.js
@@ -2,11 +2,48 @@
 $ = jQuery;
 let PEOPLE = {};
 
+// load a div for each unique face detected
+function renderFaceDiv(faceKey) {
+  var arr = Object.values(PEOPLE);
+  arr = arr.sort((a, b) => (a.timestamp > b.timestamp) ? -1 : 1);
+
+  var overlay = $("#overlay");
+  overlay.html("");
+
+  for(var index in PEOPLE) {
+    var person = PEOPLE[index];
+    var faceDiv = document.createElement("div")
+    var linkToImdb = document.createElement("a")
+    var nameOfFace = document.createTextNode(person.name)
+
+    faceDiv.style.border = "1px solid blue"
+    faceDiv.style.padding = "15pt"
+    linkToImdb.href = person.imdb_url
+    linkToImdb.target = '_blank'
+
+    // add content to faceDiv
+    faceDiv.appendChild(linkToImdb)
+    linkToImdb.appendChild(nameOfFace)
+
+    // add faceDiv to overlay
+    overlay.append(faceDiv)
+  }
+}
+
 function startRecognitionInVideo() {
   // get references to required items on screen.
   var $video = $("video");
   $video.css("position", "absolute");
   var video = $video[0];
+  // create div element to overlay video
+  var overlay = document.createElement("div");
+  overlay.id = "overlay";
+  overlay.style.position = "absolute";
+  overlay.style.opacity = "0.8";
+  overlay.style.margin = "0 auto";
+  overlay.width = $video.width();
+  overlay.height = $video.height();
+  $video.parent().append(overlay);
   // create a canvas element to convert current video frame into an still
   var canvas2 = document.createElement("canvas");
   var context2 = canvas2.getContext("2d");
@@ -39,7 +76,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
   else if (request.method == "person") {
     PEOPLE[request.data.key] = request.data;
-    console.log(PEOPLE);
+    renderFaceDiv()
   }
   sendResponse({message: "done"});
 });
